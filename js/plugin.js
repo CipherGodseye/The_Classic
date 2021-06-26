e5f5ce75e55e86a80477b0e718bbe75d244fce78
CipherGodseye <iamjoudnour@gmail.com>
2021-06-26 12:53:14 +0100

    Created The Header, Installed The BxSlider, Made The Smooth Scroll

---
+++ js/plugin.js
@@ -0,0 +1,55 @@
+/*global $, alert, console*/
+$(function () {
+
+    'use strict';
+
+    // Adjust Header Height
+
+    var myHeader = $('.header'),
+        mySlider = $('.bxslider');
+
+    myHeader.height($(window).height());
+
+    $(window).resize(function () {
+
+        myHeader.height($(window).height());
+
+    });
+
+    // Add Links Active Class
+
+    $('.links li').click(function () {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
+    // Making The BxSlider Center
+
+    mySlider.each(function () {
+
+        $(this).css('paddingTop', ($(window).height() - $('.bxslider div').height()) / 2);
+
+    });
+
+    // Trigger The BxSlider
+
+    mySlider.bxSlider({
+
+        pager: false
+
+    });
+
+    // Smooth Scroll
+
+    $('.links li a').click(function () {
+
+        $('html, body').animate({
+
+            scrollTop: $('#' + $(this).data('value')).offset().top
+
+        }, 1000);
+
+    });
+
+});
