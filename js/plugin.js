1159f1343e901edc42ce70953179f263a58ab60a
CipherGodseye <iamjoudnour@gmail.com>
2021-06-27 16:07:24 +0100

    Created Testimonial

--- js/plugin.js
+++ js/plugin.js
@@ -52,4 +52,39 @@ $(function () {
 
     });
 
+    // Testimonials Silder
+
+    (function autoSlider() {
+
+        $('.t-slider .active').each(function () {
+
+            if (!$(this).is(':last-child')) {
+
+                $(this).delay(3000).fadeOut(1000, function () {
+
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+
+                    autoSlider();
+
+                });
+
+            } else {
+
+                $(this).delay(3000).fadeOut(1000, function () {
+
+                    $(this).removeClass('active');
+
+                    $('.t-slider div').eq(0).addClass('active').fadeIn();
+
+                    autoSlider();
+
+                });
+
+            }
+
+        });
+
+    }());
+
+
 });
